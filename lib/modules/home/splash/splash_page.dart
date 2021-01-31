3d099d8ccbc4b8b3fd6153e2fc8aece248c0c4c0
Igor Pedroso <iigorpedroso@gmail.com>
2021-01-31 18:35:14 -0300

    criando tela de splash

---
+++ lib/modules/home/splash/splash_page.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () => {
+      Navigator.pushReplacementNamed(context, 'fluxo/home')
+    });
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/balance.png'),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: Text("FLUXO",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w100)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+}
