327d5be70b129312765699119e80feeb527fe9e1
Igor Pedroso <iigorpedroso@gmail.com>
2021-01-31 18:35:24 -0300

    criando tela de relatorios

---
+++ lib/modules/fluxo/reports/reports_fluxo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class ReportsFluxo extends StatefulWidget {
+  @override
+  _ReportsFluxoState createState() => _ReportsFluxoState();
+}
+
+class _ReportsFluxoState extends State<ReportsFluxo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Relatórios"),
+      ),
+    );
+  }
+}
