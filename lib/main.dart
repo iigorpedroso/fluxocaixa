f0adccb3b743e47e81877c3e541a375308474468
Igor Pedroso <iigorpedroso@gmail.com>
2021-03-07 18:19:57 -0300

    criando novas rotas no sistema

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:fluxo/modules/fluxo/register/register_type_expense.dart';
 import 'package:fluxo/modules/fluxo/register/register_type_recipe.dart';
 import 'package:fluxo/modules/fluxo/reports/reports_fluxo.dart';
 import 'package:fluxo/modules/home/login/login_page.dart';
+import 'package:fluxo/modules/home/login/register_page.dart';
 import 'package:fluxo/modules/home/splash/splash_page.dart';
 
 void main() {
@@ -23,6 +24,7 @@ void main() {
       'list/typeexpense': (context) => ListTypeExpense(),
       'list/typerecipe': (context) => ListTypeRecipe(),
       'reports': (context) => ReportsFluxo(),
+      'register': (context) => RegisterPage(),
       'register/expense': (context) => RegisterExpense(),
       'register/recipe': (context) => RegisterRecipe(),
       'register/typeexpense': (context) => RegisterTypeExpense(),
